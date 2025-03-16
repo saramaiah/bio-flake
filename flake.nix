d2172765954ae81d6e27f3b815d457523444c7c4
trm109 <trm109@case.edu>
2025-03-16 16:16:00 -0400

    fixed hdgwcna

--- flake.nix
+++ flake.nix
@@ -1,26 +1,23 @@
 {
   description = "A flake full of R packages for Bioinformatics";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Make this changable via `follows?`
-    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = {
     self,
     nixpkgs,
-    utils,
-  }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
-    in {
-      packages = {
-        hdWGCNA = import ./pkgs/hdWGCNA.nix {nixpkgs = pkgs;};
-      };
-    });
-
-  #let
-  #  forEachSystem = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
-  #  forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
-  #in {
-  #  #packages = forEachPkgs (pkgs: import ./pkgs {inherit pkgs;});
-  #};
+  }: {
+    packages = let
+      supportedSystems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      eachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f (import nixpkgs {inherit system;}));
+    in
+      eachSupportedSystem (pkgs: {
+        hdWGCNA = pkgs.callPackage ./pkgs/hdWGCNA.nix {};
+      });
+  };
 }
